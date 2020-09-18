94fe4b8102690ccf8efa5019aecf6ca70b17abbe
Rutvik shah <rutvikshah245@gmail.com>
2020-09-18 21:48:43 +0530

    Updates

---
+++ precache-manifest.68924b76156226327e3403932e222a2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7319a807342dc1319a14",
+    "url": "/Rutvikshah1/Restaurant/static/css/main.bf8d8fca.chunk.css"
+  },
+  {
+    "revision": "7319a807342dc1319a14",
+    "url": "/Rutvikshah1/Restaurant/static/js/main.1c2e8c58.chunk.js"
+  },
+  {
+    "revision": "0efd3a5b55301686fd72",
+    "url": "/Rutvikshah1/Restaurant/static/js/runtime~main.e7434bc2.js"
+  },
+  {
+    "revision": "76c77dce9a245ea2d2f4",
+    "url": "/Rutvikshah1/Restaurant/static/js/2.77fd8535.chunk.js"
+  },
+  {
+    "revision": "75ada5aedda23ede80ddce4690e0dae3",
+    "url": "/Rutvikshah1/Restaurant/static/media/chef.75ada5ae.jpg"
+  },
+  {
+    "revision": "a11c19b876f9a7b2b9b50aece56f1f19",
+    "url": "/Rutvikshah1/Restaurant/static/media/bgimg1.a11c19b8.jpg"
+  },
+  {
+    "revision": "cc0317d5195e0d85bb6900040fdd7d5e",
+    "url": "/Rutvikshah1/Restaurant/index.html"
+  }
+];
